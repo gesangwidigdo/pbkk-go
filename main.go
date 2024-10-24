2717bb78c0853425ed99174a0258bb111a9cd6fa
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-10-24 08:54:43 +0700

    array & slices

--- main.go
+++ main.go
@@ -27,6 +27,17 @@ func main() {
 	var email string
 	var userTickets uint
 
+	// declaring array with value
+	// var bookings = [50]string{"John Doe", "Jane Doe", "Alice Doe"} --> fixed size array
+
+	// array declaration
+	// var bookings [50]string
+
+	// slice declaration
+	// var bookings []string
+	// alternatives:
+	bookings := []string{}
+
 	// ask user for input
 	fmt.Print("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -41,7 +52,20 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
+	
+	// storing to array
+	// bookings[0] = firstName + " " + lastName
+
+	// storing to slices
+	bookings = append(bookings, firstName + " " + lastName)
+
+	// fmt.Printf("The whole array/slice: %v\n", bookings)
+	// fmt.Printf("The first val of array/slice: %v\n", bookings[0])
+	// fmt.Printf("The type of array/slice: %T\n", bookings)
+	// fmt.Printf("The length of array/slice: %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are our booking list: %v\n", bookings)
 }
